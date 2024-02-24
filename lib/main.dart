10bb86b2bfc0f3c82d48cd2b60fdc2ea5eccfb51
Ayishamarva7998 <ayshaaysha7998@gmail.com>
2024-02-24 14:24:16 +0530

    first commt

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_login/controller/bottombar_provider.dart';
+import 'package:firebase_login/controller/category_provider.dart';
+import 'package:firebase_login/controller/homepage_provider.dart';
+import 'package:firebase_login/controller/wishlist_provider.dart';
+import 'package:firebase_login/firebase_options.dart';
+import 'package:firebase_login/views/first_screen.dart';
+import 'package:firebase_login/views/main_page.dart';
+import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
+
+void main()async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context)=>BottomBarProvider()),
+        ChangeNotifierProvider(create: (context)=>Wishlist()),
+        ChangeNotifierProvider(create: (context)=>HomeProvider()),
+        ChangeNotifierProvider(create: (context)=>CategoryProvider()),
+      ],
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: MainPage(),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/bottombar.dart
@@ -0,0 +1,69 @@
+import 'package:firebase_login/controller/bottombar_provider.dart';
+import 'package:firebase_login/views/home.dart';
+import 'package:firebase_login/views/profile.dart';
+import 'package:firebase_login/views/storagepage.dart';
+import 'package:firebase_login/views/wishlist.dart';
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+import 'package:provider/provider.dart';
+
+class BottomNavigation extends StatefulWidget {
+  const BottomNavigation({super.key});
+
+  @override
+  State<BottomNavigation> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<BottomNavigation> {
+  @override
+    final List<Widget> _pages = [
+    HomePage(),
+    StorePage(),
+    CartPage(),
+    Profile(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final bottomProvider=Provider.of<BottomBarProvider>(context);
+    return Scaffold(
+      backgroundColor:  Color.fromARGB(255, 24, 30, 41),
+      body: _pages[bottomProvider.currentIndex],
+      bottomNavigationBar: Container( 
+        child: NavigationBar(
+          backgroundColor:  Color.fromARGB(255, 18, 23, 31),
+          selectedIndex: bottomProvider.currentIndex,
+          onDestinationSelected: (index) {
+            bottomProvider.navigatePage(index);
+          },
+          destinations: [
+            NavigationDestination(
+              icon: Icon(Iconsax.home,
+                  color:  bottomProvider.currentIndex == 0 ? Colors.white : const Color.fromARGB(255, 106, 105, 105)),
+              label: 'Home',
+            ),
+            NavigationDestination(
+              icon: Icon(Iconsax.shop,
+                  color:  bottomProvider.currentIndex == 1 ? Colors.white : const Color.fromARGB(255, 106, 105, 105)),
+              label: 'Store',
+              
+            ),
+            NavigationDestination(
+              icon: Icon(Iconsax.shopping_cart,
+                  color:  bottomProvider.currentIndex == 2 ? Colors.white : const Color.fromARGB(255, 106, 105, 105)),
+              label: 'Cart',
+            ),
+            NavigationDestination(
+              icon: Icon(Iconsax.user,
+                  color:  bottomProvider.currentIndex == 3 ? Colors.white : const Color.fromARGB(255, 106, 105, 105)),
+              label: 'User',
+      
+            ),
+          ],
+          indicatorColor: Colors.amber,
+        ),
+      ),
+    );
+  }
+}
